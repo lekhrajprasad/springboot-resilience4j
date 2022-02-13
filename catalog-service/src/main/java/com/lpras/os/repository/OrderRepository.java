035c1d9eff2e788555eabebb9ad39e533998d77d
lekhrajprasad <lekhraj.prasad@gmail.com>
2022-02-13 14:02:36 +0530

    1st commit

---
+++ catalog-service/src/main/java/com/lpras/os/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.lpras.os.repository;
+
+import com.lpras.os.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    List<Order> findByCategory(String category);
+
+
+}
